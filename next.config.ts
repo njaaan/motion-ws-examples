b2fe3823c57e472a94a5b7e71dcc6e15b297f936
njaaan <88497703+njaaan@users.noreply.github.com>
2025-02-11 14:30:54 +0100

    Fixing img base path

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // config for github pages, howto: https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/
+  /* config for github pages, howto: https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/ */
   basePath: "/motion-ws-examples",
   output: "export",
   // not sure if the strictmode is needed for that or just a good default?
-  reactStrictMode: true
+  // reactStrictMode: true
 };
 
 export default nextConfig;
