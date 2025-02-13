cc0d13e0dede6fc925ff1c2633e54f85be7b46cd
njaaan <88497703+njaaan@users.noreply.github.com>
2025-02-13 14:20:43 +0100

    Specify different asset basePath for dev and prod

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  /* config for github pages, howto: https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/ */
-  basePath: "/motion-ws-examples",
-  output: "export",
-  // not sure if the strictmode is needed for that or just a good default?
-  // reactStrictMode: true
+const isInDevMode = process.env.NODE_ENV === 'development';
+
+const nextConfig: NextConfig = { 
+  output: "export", 
+  basePath: isInDevMode ? '' :  "/motion-ws-examples",
 };
 
 export default nextConfig;
