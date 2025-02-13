48fce82270d8f9c67cc5879c9ebb001ee405b5b4
njaaan <88497703+njaaan@users.noreply.github.com>
2025-02-13 14:46:45 +0100

    Third time is the charm: Maybe basePath is handled by the yml and not needed in the config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import { NextConfig } from "next";
 
-const isInDevMode = process.env.NODE_ENV === 'development';
-
 const nextConfig: NextConfig = { 
   output: "export", 
-  basePath: isInDevMode ? '' :  "/motion-ws-examples",
 };
 
 export default nextConfig;
